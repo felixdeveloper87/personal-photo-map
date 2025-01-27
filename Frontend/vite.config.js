117d21d6957e090600571b50baf944ad47a72973
felixdeveloper87 <52666888+felixdeveloper87@users.noreply.github.com>
2025-01-27 15:43:56 +0000

    Ajustar caminho do package.json no Dockerfile.prod

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 8080, // Porta para o frontend no Docker
+    port: process.env.PORT || 8080, // Porta para o frontend no Docker
     host: true, // Permite expor para redes externas
     watch: {
       usePolling: true, // Necessário para Docker
